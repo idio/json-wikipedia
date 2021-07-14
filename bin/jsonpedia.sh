b9e15ae6faa6900b45fb10546b4fe43833d6bee3
Efstathios Charitos <stathischaritos@gmail.com>
2021-07-14 12:34:32 +0300

    spark memory and cores arguments [docker]

--- bin/jsonpedia.sh
+++ bin/jsonpedia.sh
@@ -7,7 +7,12 @@ set -e
     -input $1 \
     --output /mnt/data/output_parts \
     --lang $3 \
-    --action export-parallel
-
+    --action export-parallel \
+    --driver-memory $4 \
+    --driver-cores $5 \
+    --executor-memory $6 \
+    --executor-cores $7 \
+    --num-executors $8  
+   
 mkdir -p  $(dirname "$2")
 cat /mnt/data/output_parts/part-* > $2
